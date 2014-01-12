19e6b6faf9ae53315d758a66ce6a81776af5090d
Karl Sutt <karl@sutt.ee>
2014-01-12 13:59:11 +0000

    Scope tracking to the extension

--- deg2dec.js
+++ deg2dec.js
@@ -1,7 +1,13 @@
 "use strict";
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-34162589-2']);
+_gaq.push(['_trackPageview']);
 
 // Initialise tool when content is loaded
 (function init () {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   // register handlers
   document.addEventListener("DOMContentLoaded", function () {
     registerHandlers();
--- ga-config.js
+++
@@ -1,3 +0,0 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-34162589-2']);
-_gaq.push(['_trackPageview']);
